58153cae28b9fec738ee9fe6ebde44585bf3f9ae
Leandro Farias <leoffarias@hotmail.com>
2015-04-27 02:04:44 -0300

    checkpoint 7
    game pronto

--- js/main.js
+++ js/main.js
@@ -20,9 +20,72 @@ $(function() {
   });
 });
 
+//Sistema de msg
+var audio = $("#som")[0];
+var msgs = 1;
+
 $( "#logo-grande" ).click(function() {
-  $("#balao1").fadeIn(500).animate({top:100},3000).fadeOut(500);
+
+  if(msgs == 1) {
+    $("#balao1").fadeIn(500).animate({top:100},3000).fadeOut(500);
+    $(".btn-in").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#3268E5"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#3268E5"}, 1000);
+    $(".btn-in .plural-tit").animate({"color":"#3268E5"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#3268E5"}, 1000).animate({"color":"#ffffff"}, 1000);
+    audio.play();
+    msgs++;
+
+  } else if (msgs == 2) {
+        $("#balao2").fadeIn(500).animate({top:100},3000).fadeOut(500);
+    $(".btn-midia").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#23967F"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#23967F"}, 1000);
+    $(".btn-midia .plural-tit").animate({"color":"#23967F"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#23967F"}, 1000).animate({"color":"#ffffff"}, 1000);
+    audio.play();
+    msgs++;
+
+  } else if (msgs == 3) {
+    $("#balao3").fadeIn(500).animate({top:100},3000).fadeOut(500);
+    $(".btn-on").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#EEB211"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#EEB211"}, 1000);
+    $(".btn-on .plural-tit").animate({"color":"#EEB211"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#EEB211"}, 1000).animate({"color":"#ffffff"}, 1000);
+    audio.play();
+    msgs++;
+    
+  } else if (msgs == 4) {
+    $("#balao4").fadeIn(500).animate({top:100},3000).fadeOut(500);
+    $(".btn-out").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#009925"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#009925"}, 1000);
+    $(".btn-out .plural-tit").animate({"color":"#009925"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#009925"}, 1000).animate({"color":"#ffffff"}, 1000);
+    audio.play();
+    msgs++;
+    
+  } else if (msgs == 5) {
+    $("#balao5").fadeIn(500).animate({top:70},5000).fadeOut(500);
+    $(".btn-in").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#3268E5"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#3268E5"}, 1000);
+    $(".btn-in .plural-tit").animate({"color":"#3268E5"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#3268E5"}, 1000).animate({"color":"#ffffff"}, 1000);
+    $(".btn-on").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#EEB211"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#EEB211"}, 1000);
+    $(".btn-on .plural-tit").animate({"color":"#EEB211"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#EEB211"}, 1000).animate({"color":"#ffffff"}, 1000);
+    $(".btn-midia").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#23967F"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#23967F"}, 1000);
+    $(".btn-midia .plural-tit").animate({"color":"#23967F"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#23967F"}, 1000).animate({"color":"#ffffff"}, 1000);
+    $(".btn-out").animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#009925"}, 1000).animate({"background-color":"#ffffff"}, 1000).animate({"background-color":"#009925"}, 1000);
+    $(".btn-out .plural-tit").animate({"color":"#009925"}, 1000).animate({"color":"#ffffff"}, 1000).animate({"color":"#009925"}, 1000).animate({"color":"#ffffff"}, 1000);
+
+
+    audio.play();
+    msgs = 1;
+  }
+
 });
 
+$('.slider-parc').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false,
+  responsive: [
+  {
+    breakpoint: 622,
+    settings: {
+      slidesToShow: 1
+    }
+  }
+  ]
+});
 
 });

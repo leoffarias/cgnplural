5b8e28d368e21b8ccfc28bcd52916274be8a7912
Leandro Farias <leoffarias@hotmail.com>
2015-04-26 21:27:00 -0300

    checkpoint 5

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
-
-
+//Plax
 $('#logo-grande').plaxify({"xRange":160,"yRange":30,"invert":true})
-
 $.plax.enable();
+
+//Descer
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1200);
+        return false;
+      }
+    }
+  });
+});
 
 });
